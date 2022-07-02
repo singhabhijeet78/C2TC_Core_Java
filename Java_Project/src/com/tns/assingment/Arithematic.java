78142799ac4f077e9971466f34c2d35228a2d41e
Abhijeet Singh Rajput <singhabhijeet790@gmail.com>
2022-07-03 02:15:50 +0530

    Assingment 1

---
+++ Java_Project/src/com/tns/assingment/Arithematic.java
@@ -0,0 +1,14 @@
+package com.tns.assingment;
+
+class Arithematic {
+
+	public static void main(String[] args) {
+		
+		int x = 10, y=20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+
+	}
+
+}
