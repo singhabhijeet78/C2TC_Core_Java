107471e191f14944bf4920cdfbc8a08f3c0b9d42
Abhijeet Singh Rajput <singhabhijeet790@gmail.com>
2022-07-03 09:54:44 +0530

    Assingment1

---
+++ Java_Project/src/com/tns/assingment/Decision_making.java
@@ -0,0 +1,21 @@
+package com.tns.assingment;
+
+public class Decision_making {
+
+	public static void main(String[] args) {
+		{
+			int x = 5;
+	          if(x != 5)
+	          {
+	        	  System.out.println("Value of x is not 5");
+	        	  
+	          }
+	          else
+	          {
+	          System.out.println("Value of x is 5");
+	          }
+
+	}
+	}
+}
+
