5fdf6782d2671a4cfcde6f97e4b0026016f77d04
Abhijeet Singh Rajput <singhabhijeet790@gmail.com>
2022-07-22 12:48:03 +0530

    Commit_Thread

---
+++ Java_Project/src/com/tns/java/multithreading/ThreadDemo.java
@@ -0,0 +1,24 @@
+package com.tns.java.multithreading;
+//Creating thread using Runnable Interface
+
+class MyThread implements Runnable 
+{
+	@Override
+	public void run() {
+		for (int i=0;i<5;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
+
+public class ThreadDemo {
+
+	public static void main(String[] args) {
+		MyThread t=new MyThread();
+	Thread s=new Thread(t);
+	s.start();//start your thread 
+
+	}
+
+}
