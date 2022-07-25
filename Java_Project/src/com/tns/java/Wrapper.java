5fb4afbc668a5dfd84423b3d8fb404376e2913fb
Abhijeet Singh Rajput <singhabhijeet790@gmail.com>
2022-07-26 00:15:03 +0530

    Commit

---
+++ Java_Project/src/com/tns/java/Wrapper.java
@@ -0,0 +1,14 @@
+package com.tns.java;
+
+import java.util.ArrayList;
+
+public class Wrapper {
+
+	public static void main(String[] args)
+	{
+		ArrayList l=new ArrayList();
+		Integer i=new Integer(10);
+		l.add(i);
+		System.out.println(l);
+		}
+}
