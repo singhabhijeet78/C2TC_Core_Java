3e7e82551999e78be88eb3f16d986f5636646da7
Abhijeet Singh Rajput <singhabhijeet790@gmail.com>
2022-07-26 06:08:53 +0530

    Commit

--- Java_Project/src/com/tns/java/Wrapper.java
+++ Java_Project/src/com/tns/java/Wrapper.java
@@ -1,14 +1,22 @@
 package com.tns.java;
 
-import java.util.ArrayList;
-
 public class Wrapper {
 
 	public static void main(String[] args)
 	{
-		ArrayList l=new ArrayList();
-		Integer i=new Integer(10);
-		l.add(i);
-		System.out.println(l);
+		Boolean b=new Boolean("YES");
+		Boolean c=new Boolean("NO");
+		
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(b.equals(c));
+
+		//Float l=new Float(7.777);
+		//System.out.println(l);
+		
+		//ArrayList l=new ArrayList();
+		//Integer i=new Integer(10);//Automatic Boxing 
+		//l.add(i);
+		//System.out.println(l);
 		}
 }
